8fecb3107e314f254e87b797374f55874d574b07
Tate Frost <tatefrost96@gmail.com>
2021-11-09 16:07:41 -0700

    raising to any power working

--- calculator.js
+++ calculator.js
@@ -32,7 +32,7 @@ reader.question("What would you like to calculate?", function(input){
     console.log(num1 / num2);
   }
 
-  if (mathSymbol === '^'){
+  if (mathSymbol === 'root'){
     console.log(Math.sqrt(num1));
   }
 
@@ -40,6 +40,13 @@ reader.question("What would you like to calculate?", function(input){
     console.log(num1 * num1);
   }
 
+  if (mathSymbol === 'cube'){
+    console.log(num1 * num1 * num1);
+  }
+
+  if (mathSymbol === '^'){
+    console.log(num1 ** num2);
+  }
 	// This line closes the connection to the command line interface.
 	reader.close()
 
