796f58e54e2a132eaa59be76dd01d8b2d6c69989
Tate Frost <tatefrost96@gmail.com>
2021-11-09 16:10:26 -0700

    modulus function working

--- calculator.js
+++ calculator.js
@@ -47,6 +47,10 @@ reader.question("What would you like to calculate?", function(input){
   if (mathSymbol === '^'){
     console.log(num1 ** num2);
   }
+
+  if (mathSymbol === 'modulus'){
+    console.log(num1 % num2);
+  }
 	// This line closes the connection to the command line interface.
 	reader.close()
 
